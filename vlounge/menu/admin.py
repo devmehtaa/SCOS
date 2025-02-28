33e71e8a5595dd7b4aab87be051ca00fd806222a
devv2632 <devm2632@gmail.com>
2025-02-28 10:15:31 +0530

    recent updates

--- vlounge/menu/admin.py
+++ vlounge/menu/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import FoodItem, Order, Cart # Import your model
+from .models import FoodItem, Order, Cart, Stock, FoodIngredient
 
 # Register the model
 admin.site.register(FoodItem)
 admin.site.register(Order)
 admin.site.register(Cart)
+admin.site.register(Stock)
+admin.site.register(FoodIngredient)
